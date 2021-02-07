bb4854740926ae56d9c415f51c1bad8de1fccc2f
Shahrukh Uddin <shahrukh.uddin@juspay.in>
2021-02-07 16:32:06 +0530

    Updated code for hasPath.hs

--- Basics/hasPath.hs
+++ Basics/hasPath.hs
@@ -4,10 +4,11 @@ main = putStrLn $ boolToString $ hasPath [(1,2),(2,3),(3,2),(4,3),(4,5)] 1 3
 
 hasPath :: [(Int, Int)] -> Int -> Int -> Bool
 hasPath [] x y = x==y
-hasPath (a, b):arr source destination
-    | (a==source && b==destination) = True
-    | a==source = hasPath ((a,b):arr) b destination
-    | otherwise = False
+hasPath xs x y
+    | x==y = True
+    | otherwise =
+        let xs' = [(n,m) | (n,m) <- xs, n/=x] in
+        or [ hasPath xs' m y | (n,m) <- xs, n==x]
 
 boolToString :: Bool -> String
 boolToString True = "TRUE"
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Haskell-Practice
 Codes for practicing functional programming
+To run a program, open terminal and run the command: "stack runghc programfile.hs"
